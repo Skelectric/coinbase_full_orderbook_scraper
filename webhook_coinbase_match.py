0f63d77c0d3f8c91d9e37834d52aeb6a2d51374c
Skelectric <Skelectricity@Proton.me>
2022-07-16 19:51:44 -0400

    Refactored to place temporary dataframes into queue to be concatenated to final dataframes. Fixed logger not iterating filenames correctly. Fixed candle / candle data timing mismatch. Fixed output filenames. Refactored and cleaned up code.

--- webhook_coinbase_match.py
+++ webhook_coinbase_match.py
@@ -8,60 +8,66 @@ import signal
 import time
 import json
 import os
+import queue
 
 import pandas as pd
-# pd.options.mode.chained_assignment = None  # default='warn'
 pd.options.display.float_format = '{:.6f}'.format
 
-#======================================================================================
+# ======================================================================================
 # Webhook Parameters
 
 ENDPOINT = 'wss://ws-feed.exchange.coinbase.com'
 TICKERS = ['BTC-USD', 'ETH-USD']
-#TICKERS = ['BTC-USD', 'ETH-USD', 'SOL-USD', 'AVAX-USD', 'UNI-USD', 'SNX-USD', 'AAVE-USD', 'YFI-USD', 'XMR-USD']
+# TICKERS = ['BTC-USD', 'ETH-USD', 'SOL-USD', 'AVAX-USD', 'UNI-USD', 'SNX-USD', 'AAVE-USD', 'YFI-USD', 'XMR-USD']
 FREQUENCY = '1T'
-#FREQUENCIES = ['1T', '5T', '15T', '1H', '4H', '1D']
+# FREQUENCIES = ['1T', '5T', '15T', '1H', '4H', '1D']
 SAVE_CSV = True
 SAVE_HD5 = False
 
-#======================================================================================
+# ======================================================================================
 # Configure Logging
 
 log_f = 'webhook_coinbase_match_log'
-i = 0
-while os.path.exists(f"{log_f}_{i}.log"):
-    i += 1
-log_f = f"{log_f}_{i}.log"
+_i = 0
+while os.path.exists(rf"logs\{log_f}_{_i}.log"):
+    _i += 1
+log_f = rf"logs\{log_f}_{_i}.log"
 
 # logging.disable(logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s (%(threadName)-10s) %(message)s',
                     handlers=[
-                        logging.FileHandler(rf"logs\{log_f}s"),
+                        logging.FileHandler(log_f),
                         logging.StreamHandler()
                     ],
                     force=True)
 
-#======================================================================================
+# ======================================================================================
+
 
 class GracefulKiller:
     """Help kill threads when stop called."""
     kill_now = False
+
     def __init__(self):
         signal.signal(signal.SIGINT, self.exit_gracefully)
         signal.signal(signal.SIGBREAK, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
+
     def exit_gracefully(self, *args):
         logging.info(f"Stop triggered. Ending threads.............................")
         self.kill_now = True
 
+
 s_print_lock = Lock()
 result_lock = Lock()
+
 
 def s_print(*a, **b):
     """Thread-safe print function."""
     with s_print_lock:
         print(*a, **b)
+
 
 def main():
     threads = []
@@ -69,11 +75,11 @@ def main():
     ws = [None] * len(TICKERS)
     data_col = ["time", "ticker", "side", "size", "price"]
     candle_col = ["ticker", "candle", "open", "high", "low", "close", "volume"]
-    # create list of dataframes so that each thread can work on its own dataframe
-    data = [pd.DataFrame(columns=data_col) for i in range(len(TICKERS))]
-    candles = [pd.DataFrame(columns=candle_col) for i in range(len(TICKERS))]
+    data = pd.DataFrame(columns=data_col)
+    candles = pd.DataFrame(columns=candle_col)
+    df_queue = queue.Queue()
 
-    def websocket_thread(ws, ticker, i, data, candles):
+    def websocket_thread(ws, ticker, i, data_col, candle_col, q):
         ws[i] = create_connection(ENDPOINT)
         ws[i].send(
             json.dumps(
@@ -89,6 +95,9 @@ def main():
         )
 
         threads_keepalive[i].start()
+
+        # needed for first loop
+        last_candle = None
 
         # main websocket loop
         while not killer.kill_now:
@@ -116,56 +125,45 @@ def main():
                     _price = float(msg.get('price'))
                     s_print(f"Thread {i} - {_time} --- {_side} {_size} {_id} at ${_price:,}")
 
-                    # (re)initialize temp dataframe for easy concatenation to main dataframe
-                    df = pd.DataFrame(columns=["time", "ticker", "side", "size", "price"],
+                    # (re)initialize temp dataframe for concatenation to main dataframe
+                    df = pd.DataFrame(columns=data_col,
                                       data=[[_time, _id, _side, _size, _price]])
 
                     # numpy datetime type
                     df["time"].astype("datetime64[ns]")
 
+                    # place temp match dataframe into queue
+                    q.put(df)
+
                     candle = df["time"].dt.floor(freq=FREQUENCY)[0] # floor time at chosen frequency
-                    # # extra column to track candle
-                    # df["candle"] = candle
 
-                    if not data[i].empty:
-                        # if candle != data[i]["candle"].iloc[-1]:
-                        if candle != last_candle:
-                            # if candle is new, concat into main candles dataframe and reset temp dataframe
-                            logging.debug("-----New Candle-----")
+                    if last_candle is not None and candle != last_candle:
+                        # if candle is new, concat into main candles dataframe and reset temp dataframe
+                        logging.debug("-----New Candle-----")
 
-                            df_c = pd.DataFrame(columns=list(candles[i].head()),
-                                                data=[[ticker, candle, _open, high, low, close, volume]])
+                        df_c = pd.DataFrame(columns=candle_col,
+                                            data=[[ticker, last_candle, _open, high, low, close, volume]])
 
-                            candles[i] = pd.concat([candles[i], df_c])
-                            logging.debug(f"candle added: {df_c.to_string()}")
+                        logging.debug(df_c)
 
-                            _open = _price
-                            high = _price
-                            low = _price
-                            close = _price
-                            volume = _size
+                        # place temp candle dataframe into queue
+                        q.put(df_c)
 
-                        # if candle not new, update high, low, close, and volume
-                        else:
-                            high = max(high, _price)
-                            low = min(low, _price)
-                            close = _price
-                            volume = volume + _size
-
-                    else:  # should run upon launch and for every new candle
+                    if candle != last_candle:
                         _open = _price
                         high = _price
                         low = _price
                         close = _price
                         volume = _size
+                    else: # if candle not new, update high, low, close, and volume
+                        high = max(high, _price)
+                        low = min(low, _price)
+                        close = _price
+                        volume = volume + _size
 
-                    # logging.debug(f"candle:{candle}-open:{_open},high:{high},low:{low},close:{close},volume:{volume}")
-
-                    # concat order results back to dataframe
-                    data[i] = pd.concat([data[i], df])
                     last_candle = candle
 
-        logging.debug(f"Thread {ticker} Loop Broke!")
+        logging.debug(f"Websocket thread loop for {ticker} ended.")
 
         # close websocket
         try:
@@ -185,12 +183,33 @@ def main():
             except WebSocketConnectionClosedException:
                 pass
 
+    def handle_dataframes(q):
+        """Get temporary dataframes from queue and append to final order match and candle dataframes.
+        Todo: For every k rows, append to output files"""
+        global SAVE_CSV, SAVE_HD5
+        nonlocal data, candles
+
+        k = 10000
+        while True:
+            df = q.get()
+            first_col = df.columns[0]
+            if first_col == "time":
+                data = pd.concat([data, df])
+                # logging.debug(f"Got order match df -> placed into data")
+            elif first_col == "ticker":
+                candles = pd.concat([candles, df])
+                # logging.debug(f"Got candle df -> placed into candles")
+            q.task_done()
+
     user = api_coinbase.get_user()
+
+    worker = Thread(target=handle_dataframes, args=(df_queue,), daemon=True)
+    worker.start()
 
     # handle websocket threads
     for i, ticker in enumerate(TICKERS):
         logging.info(f"Starting websocket Thread {i+1} for {ticker}.")
-        thread = Thread(target=websocket_thread, args=(ws, ticker, i, data, candles))
+        thread = Thread(target=websocket_thread, args=(ws, ticker, i, data_col, candle_col, df_queue))
         threads.append(thread)
         thread.start()
 
@@ -203,37 +222,35 @@ def main():
         time.sleep(1)
         for thread in threads:
             if not thread.is_alive():
-                thread.join()
+                # thread.join()
                 threads.remove(thread)
                 logging.info(f"Active threads = {len(threads)}")
         if not threads:
             break
 
-    data = pd.concat(data, keys=TICKERS, ignore_index=False)
-    candles = pd.concat(candles, keys=TICKERS, ignore_index=False)
+
 
     order_count = len(data)
     candle_count = len(candles)
-
-    short_ticker_txt = ''
-    for t in TICKERS:
-        short_ticker_txt += t[:t.find("-")]
-
-    file_timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    data_filename = f"Coinbase_{FREQUENCY}_kline_{order_count}_{short_ticker_txt}_USD_{file_timestamp}."
-    candle_filename = f"Coinbase_{FREQUENCY}_kline_{candle_count}_{short_ticker_txt}_USD_{file_timestamp}"
 
     with pd.option_context('display.max_rows', 30, 'display.max_columns', 20):
         print(data)
         print(candles)
 
-    if SAVE_CSV:
-        data.to_csv(f"data\{data_filename}.csv")
-        candles.to_csv(f"data\{candle_filename}.csv")
+    # handle output filenames and save if options are selected and dataframes are not empty
+    short_ticker_txt = ','.join([t[:t.find("-")] for t in TICKERS])
+    file_timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    data_filename = f"Coinbase_{FREQUENCY}_{order_count}_order_matches_{short_ticker_txt}_USD_{file_timestamp}."
+    candle_filename = f"Coinbase_{FREQUENCY}_OHLC_{candle_count}_candles_{short_ticker_txt}_USD_{file_timestamp}"
 
-    if SAVE_HD5:
-        data.to_csv(f"data\{data_filename}.h5")
-        candles.to_csv(f"data\{candle_filename}.h5")
+    if order_count != 0 and candle_count != 0:
+        if SAVE_CSV:
+            data.to_csv(rf"data\{data_filename}.csv")
+            candles.to_csv(rf"data\{candle_filename}.csv")
+        if SAVE_HD5:
+            data.to_csv(rf"data\{data_filename}.h5")
+            candles.to_csv(rf"data\{candle_filename}.h5")
+
 
 if __name__ == '__main__':
     start_time = datetime.now()
